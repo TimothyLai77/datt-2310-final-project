f466ce4a65e56792d32f8bf1ad4dfc767c6aca2a
Tim <33475176+TimothyLai77@users.noreply.github.com>
2023-02-10 17:21:08 -0500

    added dying and respawning

---
+++ BE MY PLAYER 2/Assets/DeathScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScript : MonoBehaviour
+{
+    public GameObject startPoint;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if player collides with death object they respawn at start
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = startPoint.transform.position;
+        }
+    }
+}
