510ef4763ceccd0cbd8bd8451334774aa8af8c8d
SketchyMints <124720151+SketchyMints@users.noreply.github.com>
2023-02-12 22:24:37 -0500

    Basic Hub Attachment
    A rough version of the hub for the game.

---
+++ BE MY PLAYER 2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+
+    void Start()
+    {
+         
+    }
+
+   
+    void Update() {
+        MovementInput();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movement * moveSpeed;
+
+    }
+
+    void MovementInput ()
+    {
+        float mx = Input.GetAxisRaw("Horizontal");
+        float my = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(mx, my).normalized;
+    }
+}
