48dead5f71111ea13fd94906c038828efb5e46a7
WarrenWang02 <122489768+WarrenWang02@users.noreply.github.com>
2023-04-11 20:54:03 -0400

    background

--- BE MY PLAYER 2/Assets/Scripts/PlatformGame/TopHitDS.cs
+++ BE MY PLAYER 2/Assets/Scripts/PlatformGame/TopHitDS.cs
@@ -25,7 +25,7 @@ public class TopHitDS : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             topHitCheck.startReset();
-            Debug.Log("start reset");
+            Debug.Log("start reset");//test
         }
     }
 }
